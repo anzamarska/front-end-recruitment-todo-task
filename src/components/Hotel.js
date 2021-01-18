8d4399802d74673271a8b02859bac45de85db583
anzamarska <anzamarska@gmail.com>
2021-01-18 21:13:29 +0100

    reorganized the hotel component and styled it

--- src/components/Hotel.js
+++ src/components/Hotel.js
@@ -4,15 +4,27 @@ import "../styles/Hotel.css";
 function Hotel({ id, image, name, price, subtitle }) {
 
   return (
-    <div className="hotel">
+    <div className="hotel__container">
       <img className="hotel__image" src={image} alt="" />
       <div className="hotel__info">
         <p className="hotel__title">{name}</p>
+        <p className="hotel__subtitle">{subtitle}</p>
         <p className="hotel__price">
+          {/* per night */}
           <small>$</small>
           <strong>{price}</strong>
         </p>
       </div>
+      <div className="quantity__section">
+        <button className="button--delete">delete hotel</button>
+        <button className="button--decrease">-</button>
+        <p>0</p>
+        <button className="button--increase">+</button>
+      </div>
+      <p className="hotel__subtotalPrice">
+          <small>$</small>
+          <strong>{price}</strong>
+        </p>
     </div>
   );
 }
--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -6,18 +6,13 @@ import Hotel from "../../components/Hotel";
 export const Home = () => (
 <div className="home">
   <div className="home__container">
-   
-    <div className="home__row">
       <Hotel
         id="1"
         name="AAA hotel name"
         price={29.22}
         image=""
-        subtitle=""
+        subtitle="description of the hotel description of the hotel description of the hotel description of the hotel description of the hotel description of the hotel description of the hotel "
       />
-
-    </div>
-
   </div>
 </div>
 );
