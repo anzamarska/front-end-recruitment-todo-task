5f6baa2281ce1bbfb67ed3961c1e93d5e18360b5
anzamarska <anzamarska@gmail.com>
2021-01-18 16:09:45 +0100

    structuring the project, adding the main view and payments view

---
+++ src/views/Home/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const Home = () => (
+  <div>
+    <p>this is the home site</p>
+  </div>
+);
---
+++ src/views/Payments/Payments.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const Payments = () => (
+  <div>
+    <p>this is the payments site</p>
+  </div>
+);
