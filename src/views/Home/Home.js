f654a571132aba59c0d259f25b7b103c45f45f61
unknown <anzamarska@gmail.com>
2021-01-21 20:24:01 +0100

    add media query for hotels and form, breakpoint 400px

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import "../../styles/Home.css";
 import Hotel from "../../components/Hotel";
 import TotalPrice from "../../components/TotalPrice";
 
